6d3eeab7e50d1ceb13df1f21268f83116f3a6fe0
PK4U_S <Stepanchuk.ei@gmail.com>
2022-09-09 13:49:31 +0300

    Initial commit

---
+++ functional_programming/src/main/java/com/pk4us/functional_programming/filter_and_map/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.pk4us.functional_programming.filter_and_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.github.javafaker.Faker
+import com.pk4us.functional_programming.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
