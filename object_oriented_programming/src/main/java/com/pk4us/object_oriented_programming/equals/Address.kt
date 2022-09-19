32350689d5605a4819353be3b19652fb16049a41
PK4U_S <Stepanchuk.ei@gmail.com>
2022-09-19 14:01:09 +0300

    4. Equals

---
+++ object_oriented_programming/src/main/java/com/pk4us/object_oriented_programming/equals/Address.kt
@@ -0,0 +1,8 @@
+package com.pk4us.object_oriented_programming.equals
+
+class Address( val city: String? = null, val street: String? = null, val numberOfHouse: Int? = null) {
+
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+}
---
+++ object_oriented_programming/src/main/java/com/pk4us/object_oriented_programming/equals/Equals.kt
@@ -0,0 +1,38 @@
+package com.pk4us.object_oriented_programming.equals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.pk4us.object_oriented_programming.R
+
+class Equals : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_equals)
+
+        val kyiv = Address("kyiv","rubana",113)
+        val kyiv2 = Address("kyiv","rubana",113)
+        val kyiv3 = kyiv
+
+
+        if (kyiv == kyiv2){
+            Log.d("MyLog", "Yes")
+        }else{
+            Log.d("MyLog", "No")
+        }
+
+
+        if (kyiv == kyiv3){
+            Log.d("MyLog", "Yes")
+        }else{
+            Log.d("MyLog", "No")
+        }
+
+
+        if (kyiv.equals(kyiv3)){
+            Log.d("MyLog", "Yes")
+        }else{
+            Log.d("MyLog", "No")
+        }
+    }
+}
